d2bbdbb1bfdb4c10a0c846fe5e5a8af6808016cb
De1mandos <matess.mazanek@gmail.com>
2021-09-28 14:18:34 +0200

    Added header

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
-import logo from './logo.svg';
+import Header from './components/UI/header/Header';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
+    <div>
+      <Header></Header> 
     </div>
   );
 }
