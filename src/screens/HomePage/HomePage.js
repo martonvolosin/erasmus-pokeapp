5c0b5ca344e27659582eb13b9cc9ad9942812432
MartinCechh <91527982+MartinCechh@users.noreply.github.com>
2021-09-29 13:39:55 +0200

    Update HomePage.js

--- src/screens/HomePage/HomePage.js
+++ src/screens/HomePage/HomePage.js
@@ -3,11 +3,20 @@
  */
  import React, {useState} from 'react';
  import CheckBox from '../../components/FilterComponents/checkbox/CheckBox';
+ import PokeType from '../../components/FilterComponents/poketype/PokeType';
  import './HomePage.css'
 
 
   function HomePage() {
 
+     const [filterType, setFilterType] = useState('All');
+
+ 
+     function typeFilter(selectedType) {
+ 
+         setFilterType(selectedType);
+     };
+ 
       function collectCheck(checked) {
 
           console.log(checked);
@@ -16,6 +25,7 @@
       return (
           <div className='page'>
               <div className='left'>
+                  <PokeType typeChange={typeFilter} selected={filterType}/>
                   <CheckBox press={collectCheck}/> 
               </div>
           </div>
