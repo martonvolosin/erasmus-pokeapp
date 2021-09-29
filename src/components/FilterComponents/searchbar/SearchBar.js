f786d55cd175ca6ff3f23a1daac11df8f4975360
Jiří Prokop <it-jiriprokop@stredniskola.net>
2021-09-29 14:14:41 +0200

    searchbar

--- src/components/FilterComponents/searchbar/SearchBar.js
+++ src/components/FilterComponents/searchbar/SearchBar.js
@@ -1,3 +1,17 @@
 /**
  * This SearchBar will be used for searching pokemons by name etc.
  */
+ import './SearchBar.css';
+ 
+ const SearchBar = () => {
+     return (
+         <form action="/" method="get" className="leftAlign">
+             <label htmlFor="header-search">
+                 <span className="hidden"></span>
+            </label>
+            <input type="text" id="header-search" placeholder="Search the pokemons" name="s"/>
+            
+         </form>
+     ); 
+ };
+ export default SearchBar;
